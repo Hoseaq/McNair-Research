0436f778e587608ee0972d3e907ad355d19ab846
Ron Belmont <belmonrj@gmail.com>
2021-06-16 11:50:00 -0400

    Removed excess whitespace as a test...

--- tracks.C
+++ tracks.C
@@ -13,31 +13,31 @@ void tracks()
   TProfile* c24b = (TProfile*)file->Get("nfvtxt_ac_fvtxsfvtxn_tracks_c24b");
   TProfile* c22a = (TProfile*)file->Get("nfvtxt_ac_fvtxs_tracks_c22");
   TProfile* c22b = (TProfile*)file->Get("nfvtxt_ac_fvtxn_tracks_c22");
-  
+
   //Retrieves specific files from local root file
   //TH1D histograms contain 1 double per bin
   //TH1D* creates a pointer to a histogram
   TH1D* c22x = c22->ProjectionX("c22x");
-  TH1D* c24x = c24->ProjectionX("c24X"); 
+  TH1D* c24x = c24->ProjectionX("c24X");
   c22x -> Multiply(c22x);
   c22x -> Scale(2);
   c24x -> Add(c22x,-1);
   c24x -> Draw();
   c1.Print("c24x.png");
   //c2{4} histogram is now drawn
-  
+
   // creation of cn{4} ab|ab histogram
   TH1D* c24bx = c24b ->ProjectionX("c24bx");
-  TH1D* c22ax = c22a->ProjectionX("c22ax"); 
+  TH1D* c22ax = c22a->ProjectionX("c22ax");
   TH1D* c22bx = c22b->ProjectionX("c22bx");
   if ( c22ab == NULL )
     {
       cout << "cannot find c22ab" <<  endl;
       return;
     }
-  
+
   TH1D* c22abx = c22ab->ProjectionX("c22abx");
-  
+
   c22abx -> Multiply(c22abx);
   c22ax -> Multiply(c22bx);
   c22ax -> Add(c22abx, -1);
@@ -45,7 +45,7 @@ void tracks()
   c24bx -> Draw();
   c1.Print("c24bx.png");
   //cn{4}ab|ab has been drawn
-  
+
 
   //Creation of cn{4}aa|bb histogram
   TH1D* c24ax  = c24a -> ProjectionX("c24ax");
@@ -54,13 +54,6 @@ void tracks()
   c24ax -> Draw();
   c1.Print("c24ax.png");
   //cn{4}aa|bb histogram has been drawn
-  
+
   return;
 }
-
-
-
-
-
-
-
