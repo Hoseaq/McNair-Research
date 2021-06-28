fc1c267c93a564e382d27d890437b9230fa1e4fd
Ron Belmont <belmonrj@gmail.com>
2021-06-28 15:57:53 -0400

    Working on tracks...

--- tracks.C
+++ tracks.C
@@ -4,8 +4,11 @@ void tracks()
 
 {
   TCanvas c1("c1","");
+
+  // --- open the file
   TFile* file = TFile::Open("quentin.root","read");
-  //opens local root file
+
+  // --- get the TProfile histograms from the file
   TProfile* c22 = (TProfile*)file->Get("nfvtxt_ac_fvtxc_tracks_c22");
   TProfile* c24 = (TProfile*)file->Get("nfvtxt_ac_fvtxc_tracks_c24");
   TProfile* c22ab = (TProfile*)file->Get("nfvtxt_ac_fvtxsfvtxn_tracks_c22");
@@ -14,11 +17,27 @@ void tracks()
   TProfile* c22a = (TProfile*)file->Get("nfvtxt_ac_fvtxs_tracks_c22");
   TProfile* c22b = (TProfile*)file->Get("nfvtxt_ac_fvtxn_tracks_c22");
 
+  // --- make TH1D histograms from the TProfiles to do mathematical operations on them
+
+  TH1D* c22x = c22->ProjectionX("c22x");
+  TH1D* c24x = c24->ProjectionX("c24x");
+
+  TH1D* c22ax = c22a->ProjectionX("c22ax");
+  TH1D* c22bx = c22b->ProjectionX("c22bx");
+  TH1D* c22abx = c22ab->ProjectionX("c22abx");
+  TH1D* c24ax  = c24a -> ProjectionX("c24ax");
+  TH1D* c24bx = c24b ->ProjectionX("c24bx");
+
+  // --- make clones for rebinning purposes prior to doing any math operations
+  TH1D* c22x_rb1 = (TH1D*)c22x->Clone("c22x_rb1");
+  TH1D* c24x_rb1 = (TH1D*)c24x->Clone("c24x_rb1");
+  TH1D* c22x_rb2 = (TH1D*)c22x->Clone("c22x_rb2");
+  TH1D* c24x_rb2 = (TH1D*)c24x->Clone("c24x_rb2");
+
+  // --- math operations for full event cumulant
   //Retrieves specific files from local root file
   //TH1D histograms contain 1 double per bin
   //TH1D* creates a pointer to a histogram
-  TH1D* c22x = c22->ProjectionX("c22x");
-  TH1D* c24x = c24->ProjectionX("c24x");
   c22x -> Multiply(c22x);
   c22x -> Scale(2);
   c24x -> Add(c22x,-1);
@@ -26,18 +45,9 @@ void tracks()
   c1.Print("c24x.png");
   //c2{4} histogram is now drawn
 
-  // creation of cn{4} ab|ab histogram
-  TH1D* c24bx = c24b ->ProjectionX("c24bx");
-  TH1D* c22ax = c22a->ProjectionX("c22ax");
-  TH1D* c22bx = c22b->ProjectionX("c22bx");
-  if ( c22ab == NULL )
-    {
-      cout << "cannot find c22ab" <<  endl;
-      return;
-    }
 
-  TH1D* c22abx = c22ab->ProjectionX("c22abx");
 
+  // --- math operations for subevent cumulant ab|ab
   c22abx -> Multiply(c22abx);
   c22ax -> Multiply(c22bx);
   c22ax -> Add(c22abx, -1);
@@ -47,8 +57,9 @@ void tracks()
   //cn{4}ab|ab has been drawn
 
 
+
+  // --- math operations for subevent cumulant aa|bb
   //Creation of cn{4}aa|bb histogram
-  TH1D* c24ax  = c24a -> ProjectionX("c24ax");
   c22abx -> Scale(2);
   c24ax -> Add(c22abx, -1);
   c24ax -> Draw();
@@ -58,9 +69,7 @@ void tracks()
   //creation of single histogram
 
 
-  THStack *hs = new THStack("hs", "Superimposed histograms");
-
-  //  THStack *hs = new THStack("hs","");
+  THStack *hs = new THStack("hs", "");
   hs -> Add(c24ax);
   hs -> Add(c24bx);
   hs -> Add(c24x);
@@ -75,27 +84,48 @@ void tracks()
   //Rebinning of historgrams: seperate rebinning of <4> and <2> histograms
 
   int rebin = 2;
-  c22x -> Rebin(rebin);
-  c22x -> Scale(1.0/rebin);
-  c24x -> Rebin(rebin);
-  c24x -> Scale(1.0/rebin);
-  c22x -> Multiply(c22x);
-  c22x -> Scale(2);
-  c24x -> Add(c22x,-1);
-  c24x -> Draw();
+
+  c22x_rb1 -> Rebin(rebin);
+  c22x_rb1 -> Scale(1.0/rebin);
+  c24x_rb1 -> Rebin(rebin);
+  c24x_rb1 -> Scale(1.0/rebin);
+  c22x_rb1 -> Multiply(c22x_rb1);
+  c22x_rb1 -> Scale(2);
+  c24x_rb1 -> Add(c22x_rb1,-1);
+  c24x_rb1 -> Draw();
   c1.Print("c24rb1.png");
 
   //Rebinned seperate  c2{4} histogram
 
   //Rebinning of histograms: merged
-  c22x -> Multiply(c22x);
-  c22x -> Scale(2);
-  c24x -> Add(c22x,-1);
-  c24x -> Rebin(rebin);
-  c24x -> Scale(1.0/rebin);
-  c24x -> Draw();
+  c22x_rb2 -> Multiply(c22x_rb2);
+  c22x_rb2 -> Scale(2);
+  c24x_rb2 -> Add(c22x_rb2,-1);
+  c24x_rb2 -> Rebin(rebin);
+  c24x_rb2 -> Scale(1.0/rebin);
+  c24x_rb2 -> Draw();
   c1.Print("c24rb2.png");
-  
+
+  // --- now drawing the different rebinnings together
+  c24x->SetMarkerStyle(kOpenCircle);
+  c24x->SetMarkerColor(kBlack);
+  c24x->SetLineColor(kBlack);
+  c24x_rb1->SetMarkerStyle(kOpenDiamond);
+  c24x_rb1->SetMarkerColor(kBlue);
+  c24x_rb1->SetLineColor(kBlue);
+  c24x_rb2->SetMarkerStyle(kOpenCross);
+  c24x_rb2->SetMarkerColor(kRed);
+  c24x_rb2->SetLineColor(kRed);
+  TH2D hdummy("hdummy","",1,0,50,1,-1e-4,1e-4);
+  hdummy.Draw();
+  hdummy.GetXaxis()->SetTitle("N_{tracks}^{FVTX}");
+  hdummy.GetYaxis()->SetTitle("c_{2}{4}");
+  c24x -> Draw("same ex0p");
+  c24x_rb1 -> Draw("same ex0p");
+  c24x_rb2 -> Draw("same ex0p");
+  c1.Print("c24_rebin_comparison.png");
+
 
   return;
+
 }
